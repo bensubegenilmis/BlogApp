1d9d4f969a613dab50b8a623ccd8015bf8b4796c
Bensu Beğenilmiş <bensubegenilmiss@gmail.com>
2024-01-30 15:54:15 +0300

    new

--- context/BlogContext.js
+++ context/BlogContext.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState, useReducer } from "react";
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+    switch(action.type)
+    {
+        case 'add_blogbost':
+            return [...state, {title:'Angular'}];
+            default:
+                return state;
+    }
+}
 
-export const BlogProvider = ({children}) => {
 
-    const blogPosts = [{title: 'React Native'}, {title: 'Jacascript'}]
+export const BlogProvider = ({ children }) => {
+    const [blogPosts, dispatch] = useReducer(blogReducer, 
+        [{ title: 'React Native' },
+         { title: 'Jacascript' },]);
+  /*   const [blogPosts, setBlogPosts] = useState([
+        { title: 'React Native' },
+        { title: 'Jacascript' },
+        ]); */
 
-    return <BlogContext.Provider value={blogPosts} >{children}</BlogContext.Provider>
+    const addBlogPost = () => {
+        dispatch({type: 'add_blogbost'});
+        //setBlogPosts([...blogPosts, {title: 'Vue Js'}]);
+
+    }
+
+
+    return <BlogContext.Provider value={{data:blogPosts , addBlogPost}} >
+        {children}</BlogContext.Provider>
 };
 
 export default BlogContext;
